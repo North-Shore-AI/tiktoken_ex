a6ef2440e2990ed294c689482a57ff97bcd200e9
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-03-10 11:25:56 -1000

    chore: upgrade ex_doc to v0.40.1
    Update ex_doc dependency to version 0.40.1 in mix.exs and regenerate
    the mix.lock file to include the latest version and updated dependencies.

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule TiktokenEx.MixProject do
       {:jason, "~> 1.4"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.30", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.40.1", only: :dev, runtime: false},
       {:stream_data, "~> 1.1", only: :test}
     ]
   end
